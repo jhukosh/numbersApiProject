600009adf6a69f03079bd7d0a30811325d80cc7c
Julia Hu <juliahukoshi@gmail.com>
2019-05-31 19:58:54 +0200

    WIP saving others routes to another branch

--- src/app/services/pages-communication.service.ts
+++ src/app/services/pages-communication.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Facts } from 'src/app/models/fact.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PagesCommunicationService {
-  facts = [];
+  facts: Facts[] = localStorage.getItem('facts') ? JSON.parse(localStorage.getItem('facts')) : [];
 
   constructor() { }
 
   getFacts(facts){
     this.facts = facts;
+    this.saveToLocalStorage();
+    console.log(this.facts);
   }
 
   sendFacts(){
     return this.facts
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('facts', JSON.stringify(this.facts));
   }
 
 }
