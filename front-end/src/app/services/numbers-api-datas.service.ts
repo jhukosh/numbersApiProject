d5ba8a8780130ed4889fca0055448c1d343b4125
Julia Hu <juliahukoshi@gmail.com>
2019-06-01 16:53:20 +0200

    added favicon

--- front-end/src/app/services/numbers-api-datas.service.ts
+++ front-end/src/app/services/numbers-api-datas.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -14,6 +15,17 @@ export class NumbersApiDatasService {
   
   getDateFacts(date): Observable<any> {
     const url =`${this.dateUrl}/${date}`;
-    return this.http.get(url);
+    return this.http.get(url).pipe(
+    catchError(this.handleError([]))
+    );
   }
+
+  private handleError<T> (result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log error to console
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
 }
